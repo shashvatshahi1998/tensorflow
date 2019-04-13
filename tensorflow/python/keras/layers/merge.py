d12c5cf0adce73632379416b8eef96358916e567
shashvatshahi1998 <dracshahi@gmail.com>
2019-04-13 12:55:26 +0530

    Changes done

--- tensorflow/python/keras/layers/merge.py
+++ tensorflow/python/keras/layers/merge.py
@@ -236,6 +236,7 @@ class Add(_Merge):
       input2 = keras.layers.Input(shape=(32,))
       x2 = keras.layers.Dense(8, activation='relu')(input2)
       added = keras.layers.Add()([x1, x2])  # equivalent to added = keras.layers.add([x1, x2])
+      
       out = keras.layers.Dense(4)(added)
       model = keras.models.Model(inputs=[input1, input2], outputs=out)
   ```
